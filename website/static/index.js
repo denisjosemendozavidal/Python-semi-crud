c2d049f5a815955a618db7e93c960c08dfd71ef1
denisjosemendozavidal <denismendozavidal@gmail.com>
2022-10-20 16:35:20 -0500

    first commit

---
+++ website/static/index.js
@@ -0,0 +1,10 @@
+function deleteNote(noteId) {
+    console.log("clicked");
+    
+    fetch("/delete-note", {
+      method: "POST",
+      body: JSON.stringify({ noteId: noteId }),
+    }).then((_res) => {
+      window.location.href = "/";
+    });
+  }
